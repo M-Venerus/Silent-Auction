2a562ab07a5644e826549473396ceed075505455
M-Venerus <mikey.motorist@outlook.com>
2023-03-14 16:11:51 -0400

    Create initial program files (.gitignore, README, silen_auction). Outline TODO in silent auction codespace

---
+++ README.md
@@ -0,0 +1,4 @@
+Silent Auction Program
+
+1 input at a time. Allows user to input client bids until auction is finished.
+Produces record of highest bid when auction is closed.
---
+++ silent_auction.py
@@ -0,0 +1,39 @@
+# Silent Auction
+
+# TODO Import Hammer ASCII Art and print
+
+
+
+# TODO Print a greeting
+
+
+
+# TODO Create a function that will take 2 arguments: Name and Bid Amount.
+#      Function will add them to a dictionary
+
+
+
+# TODO Input - Ask for client name
+
+
+
+# TODO Input - Ask for bid $
+
+
+
+# TODO Create an empty dictionary for Name and Bid
+
+
+
+# TODO Call function to input Name and Bid into dictionary
+
+
+
+# TODO Input - Is there another bidder? Yes or No, tied to while loop above the function.
+
+
+
+# TODO When while loop above ends, find and print highest bidder from dictionary.
+
+
+
